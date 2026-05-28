7952fa8f7d669b83200bed7749a8e07708fe35b3
amadeusantos <amadeusantos31@gmail.com>
2026-05-28 14:53:54 -0300

    chore: remove dependencies injections from Program.cs

---
+++ Bifrost/Config/DependencyInjectionConfig.cs
@@ -0,0 +1,30 @@
+using Bifrost.Core.Adapter;
+using Bifrost.Core.Port.Repository;
+using Bifrost.Core.Service;
+using Bifrost.Infrastructure.Persistence;
+using Bifrost.Infrastructure.Persistence.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bifrost.Config;
+
+public static class DependencyInjectionConfig
+{
+    public static void AddDependencyInjection(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(connectionString));
+
+        services.AddTransient<ICourseRepository, CourseRepository>();
+        services.AddScoped<ICourseService, CourseService>();
+
+        services.AddTransient<IAssessmentSeasonRepository, AssessmentSeasonRepository>();
+        services.AddScoped<IAssessmentSeasonService, AssessmentSeasonService>();
+
+        services.AddTransient<IUserRepository, UserRepository>();
+        services.AddScoped<IUserService, UserService>();
+
+        services.AddTransient<ICoordinationRepository, CoordinationRepository>();
+        services.AddScoped<ICoordinationService, CoordinationService>();
+    }
+}
